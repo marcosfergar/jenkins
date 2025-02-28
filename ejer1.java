d4a3f9591c37036659d73c36ac56cc1871ca1bca
MARCOS FERNÁNDEZ GARCÍA <marcos.fergar@educa.jcyl.es>
2025-02-28 10:43:18 +0100

    fallo

--- ejer1.java
+++ ejer1.java
@@ -3,7 +3,7 @@ public class ejer1{
 	public static void main(String[] args){
 		Scanner teclado=new Scanner(System.in);
 		int edad;
-		System.out.println("Dime tu edad");
+		Systout.println("Dime tu edad");
 			edad=teclado.nextInt();
 		if (edad>=18){
 			System.out.println("Eres mayor de edad");
